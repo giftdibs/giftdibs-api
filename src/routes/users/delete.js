586dc06dedf295048181e26da52d318765382a93
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-24 15:09:18 -0500

    Removed comment

--- src/routes/users/delete.js
+++ src/routes/users/delete.js
@@ -7,7 +7,6 @@ function deleteUser(req, res, next) {
     .confirmUserOwnership(req.params.userId, req.user._id)
     .then((user) => user.remove())
     .then(() => {
-      // TODO: Remove wish lists, gifts, dibs, friendships owned by this user.
       res.json({
         message: 'Your account was successfully deleted. Goodbye!'
       });
