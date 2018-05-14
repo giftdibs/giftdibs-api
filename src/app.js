28f58379a3378235854e87293675bdb60e0fff29
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-05-13 22:38:37 -0400

    Adjusted

--- src/app.js
+++ src/app.js
@@ -2,26 +2,19 @@ require('./shared/environment').applyEnvironment();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
+const cors = require('cors');
 
 const db = require('./database');
 db.connect();
 
 const app = express();
-app.all('*', (req, res, next) => {
-  console.log('origin:', req.get('origin'));
-  // const origin = req.get('origin');
-  res.header('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN);
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-// console.log('Adding CORS access for:', process.env.ALLOW_ORIGIN);
-// app.use(cors({
-//   methods: 'GET,POST,PATCH,DELETE,OPTIONS',
-//   optionsSuccessStatus: 200,
-//   origin: process.env.ALLOW_ORIGIN
-// }));
+console.log('Adding CORS access for:', process.env.ALLOW_ORIGIN);
+app.use(cors({
+  methods: 'GET,POST,PATCH,DELETE,OPTIONS',
+  optionsSuccessStatus: 200,
+  origin: process.env.ALLOW_ORIGIN
+}));
+app.options('*', cors());
 app.set('port', process.env.PORT);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
