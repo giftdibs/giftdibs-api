e12c4ad7a5da98bf194e5f89d8366c92e622ad80
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-19 13:11:17 -0400

    Added logs

--- src/shared/environment.js
+++ src/shared/environment.js
@@ -1,12 +1,13 @@
-const dotenv = require('dotenv');
 const logger = require('./logger');
 
 const applyEnvironment = () => {
   // Production environments should set their own values.
   if (process.env.NODE_ENV === 'production') {
+    logger.info('Environment configuration is not parsed in production mode.');
     return;
   }
 
+  const dotenv = require('dotenv');
   const filePath = 'config.env';
   const result = dotenv.config({
     path: filePath
