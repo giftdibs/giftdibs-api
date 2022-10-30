54de376267a1c40e0f23e05a691c27159952dc12
Steve Brush <stevebrush@aol.com>
2022-10-30 19:19:06 -0400

    add support for node 16

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const app = require('./src/app');
 
 // The `app` is referenced in firebase.json `rewrites`.
 const functions = require('firebase-functions');
-exports.app = functions.https.onRequest(app);
+exports.app = functions.runWith({
+  timeoutSeconds: 300,
+  memory: '1GB'
+}).https.onRequest(app);
