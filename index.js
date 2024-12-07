bbdaaa4a7329c9ac34573a3df1684c2f6e5605bd
Steve Brush <stevebrush@aol.com>
2024-12-07 07:42:59 -0500

    use npx

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = require('./src/app');
 
 // The `app` is referenced in firebase.json `rewrites`.
 const functions = require('firebase-functions');
+
 exports.app = functions
   .runWith({
     timeoutSeconds: 300,
